d664421198f9447ccc30afba9c4abe7ffcc01e26
Remus Lazar <remus@lazar.info>
2017-09-22 20:12:20 +0200

    Added Unit Tests

--- Tests/XLIFFMergeToolTests/XLIFFMergeToolTests.swift
+++ Tests/XLIFFMergeToolTests/XLIFFMergeToolTests.swift
@@ -3,12 +3,32 @@ import XCTest
 
 class XLIFFMergeToolTests: XCTestCase {
 
-    func testExample() {
-        XCTAssertTrue(true)
+    var source: XliffFile!
+    var merge: XliffFile!
+    var bundle: Bundle!
+
+    override func setUp() {
+        super.setUp()
+        self.bundle = Bundle(for: type(of: self))
+        print(bundle.bundlePath)
+        source = try! XliffFile(xmlURL: bundle.url(forResource: "messages", withExtension: "xlf", subdirectory: "fixtures")!)
+        merge = try! XliffFile(xmlURL: bundle.url(forResource: "messages.de", withExtension: "xlf", subdirectory: "fixtures")!)
+    }
+    
+    func testMergeExistingTranslations() {
+        source.mergeExistingTranslations(from: merge)
+        let expected = try! XliffFile(xmlURL: bundle.url(forResource: "messages-merged", withExtension: "xlf", subdirectory: "fixtures")!)
+        XCTAssertEqual(source.xmlData, expected.xmlData)
     }
 
+    func testMergeTargetIntoSource() {
+        source.mergeTargetIntoSource(from: merge)
+        let expected = try! XliffFile(xmlURL: bundle.url(forResource: "messages-target-into-source", withExtension: "xlf", subdirectory: "fixtures")!)
+        XCTAssertEqual(source.xmlData, expected.xmlData)
+    }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testMergeExistingTranslations", testMergeExistingTranslations),
+        ("testMergeTargetIntoSource", testMergeTargetIntoSource),
     ]
 }
