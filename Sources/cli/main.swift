d67a77dee8d0c4cf7b7b7aaadf153a8f50fd7719
Remus Lazar <remus@lazar.info>
2017-09-22 14:27:28 +0200

    Use the CommandLineKit to make the cli more sexy

--- Sources/cli/main.swift
+++ Sources/cli/main.swift
@@ -8,20 +8,50 @@
 
 import Foundation
 import XLIFFMergeTool
+import CommandLineKit
 
-guard CommandLine.argc == 3 else {
-    print("Usage: \(CommandLine.arguments.first!) <devlang.xliff> <translated.xliff>")
-    exit(1)
+let cli = CommandLineKit.CommandLine()
+
+enum Operation: String {
+    case mergeTarget  = "mergeExistingTranslations"
+    case mergeTargetIntoSource = "mergeTargetIntoSource"
+}
+
+let devLangPath = StringOption(shortFlag: "d", longFlag: "dev-lang", required: true,
+                               helpMessage: "Path to the (auto-generated) Development Language XLIFF File")
+let mergePath = StringOption(shortFlag: "m", longFlag: "merge-from", required: true,
+                             helpMessage: "Path to the (partially) translated XLIFF File")
+let help = BoolOption(shortFlag: "h", longFlag: "help",
+                      helpMessage: "Prints a help message.")
+let op = EnumOption<Operation>(shortFlag: "o", longFlag: "operation", required: true,
+                               helpMessage: "Merge operation: mergeExistingTranslations | mergeTargetIntoSource")
+
+cli.setOptions(devLangPath, mergePath, help, op)
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+guard let operation = op.value else {
+    exit(EX_USAGE)
 }
 
 do {
-    let devlangXML = try XliffFile(xmlURL: URL(fileURLWithPath: CommandLine.arguments[1]))
-    let translatedXML = try XliffFile(xmlURL: URL(fileURLWithPath: CommandLine.arguments[2]))
+    let devlangXML = try XliffFile(xmlURL: URL(fileURLWithPath: devLangPath.value!))
+    let translatedXML = try XliffFile(xmlURL: URL(fileURLWithPath: mergePath.value!))
     
     print("Developer Language XLIFF loaded: \(devlangXML)")
     print("Translated Language XLIFF loaded: \(translatedXML)")
 
-    devlangXML.mergeTargetIntoSource(from: translatedXML)
+    switch operation {
+    case .mergeTarget:
+        devlangXML.mergeExistingTranslations(from: translatedXML)
+    case .mergeTargetIntoSource:
+        devlangXML.mergeTargetIntoSource(from: translatedXML)
+    }
     
     let output = String(data: devlangXML.xmlData, encoding: .utf8)!
     print(output)
